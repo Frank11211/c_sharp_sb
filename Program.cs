3777f84c0ec167df4447cd5fadbbf0cd9e95dc66
Frank Lee <wenfung11211@gmail.com>
2023-09-20 19:58:50 +0800

    Add Feature addition, minus, multiply, divide and percent

--- Program.cs
+++ Program.cs
@@ -5,35 +5,36 @@ namespace N_Main_Execute
 {   
     class MainExecute{
         
-        static void Main(string []args){
-            
+        // static -> call method without create object.
+        public static void AskMath(){
+
             // Access into method
             MathBasic cal = new MathBasic();
-
-            //int num1 = 14;
-            //int num2 = 29;
-
-            //Console.WriteLine(cal.AddNum(num1, num2));      // 43
-            //Console.WriteLine(cal.MinusNum(num1, num2));    // -15
-            //Console.WriteLine(cal.MultiNum(num1, num2));    // 406
-            //Console.WriteLine(cal.DivNum(num1, num2));      // 0.4287....   
-
             
             // Gather user input
-            Console.Write("Input first digit :");
-            double temp1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input first digit : ");
+            cal.Temp1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Input second digit :");
-            double temp2 = Convert.ToDouble(Console.ReadLine());
-            
+            Console.Write("Input second digit : ");
+            cal.Temp2 = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine(" ") ;
             Console.WriteLine("Result of all Mathematics") ;
             Console.WriteLine(" ") ;
-            Console.WriteLine(cal.AddNum(temp1, temp2));      
-            Console.WriteLine(cal.MinusNum(temp1, temp2));    
-            Console.WriteLine(cal.MultiNum(temp1, temp2));    
-            Console.WriteLine(cal.DivNum(temp1, temp2));        
+            Console.WriteLine("First Digit :" + cal.Temp1);
+            Console.WriteLine("Second Digit :" + cal.Temp2);
             Console.WriteLine(" ") ;
+            Console.WriteLine("The result for Add       : "+ cal.AddNum(cal.Temp1, cal.Temp2));      
+            Console.WriteLine("The result for Subtract  : "+ cal.MinusNum(cal.Temp1, cal.Temp2));    
+            Console.WriteLine("The result for Multiply  : "+ cal.MultiNum(cal.Temp1, cal.Temp2));    
+            Console.WriteLine("The result for Divide    : "+ cal.DivNum(cal.Temp1, cal.Temp2));         
+            Console.WriteLine(" ") ;
+
+        }
+        
+        static void Main(string []args){
+            
+          AskMath();
     
         }
     }
