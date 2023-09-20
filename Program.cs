523eaec9560a34147ff77fcd16eda03edc992310
Frank Lee <wenfung11211@gmail.com>
2023-09-20 20:58:30 +0800

    don't accept this push

--- Program.cs
+++ Program.cs
@@ -18,6 +18,9 @@ namespace N_Main_Execute
             Console.Write("Input second digit : ");
             cal.Temp2 = Convert.ToDouble(Console.ReadLine());
 
+            Console.Write("Input Percentage if needed : ");
+            cal.Temp_Percent = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine(" ") ;
             Console.WriteLine("Result of all Mathematics") ;
             Console.WriteLine(" ") ;
@@ -29,7 +32,9 @@ namespace N_Main_Execute
             Console.WriteLine("The result for Multiply  : "+ cal.MultiNum(cal.Temp1, cal.Temp2));    
             Console.WriteLine("The result for Divide    : "+ cal.DivNum(cal.Temp1, cal.Temp2));         
             Console.WriteLine(" ") ;
-
+            Console.WriteLine("Percentage you set is    : "+ cal.Temp_Percent +" %");
+            Console.WriteLine("After percentage is      : "+ cal.ConvertPercentToNum(cal.AddNum(cal.Temp1, cal.Temp2), cal.Temp_Percent));
+            
         }
         
         static void Main(string []args){
